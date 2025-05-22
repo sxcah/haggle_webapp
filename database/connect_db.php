982146b315194ec6b8a0e298145a8a76a6814c06
Mikko <145858615+sxcah@users.noreply.github.com>
2025-05-22 14:41:52 +0800

    Add files via upload

--- database/connect_db.php
+++ database/connect_db.php
@@ -2,10 +2,10 @@
 include 'variables.php';
 // Database connection
 
-$conn = new mysqli($servername, $username, $password, $database, $port);
+$conn = new mysqli($servername, $username, $password, $database, $second_port);
 
 if ($conn->connect_error) {
-    $conn = new mysqli($servername, $username, $password, $database, $port);
+    $conn = new mysqli($servername, $username, $password, $database, $second_port);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
